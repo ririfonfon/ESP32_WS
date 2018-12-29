63561ac62653b8a191952afa18a0508627e75756
RIRI <ririfonfon10@gmail.com>
2018-12-29 10:11:23 +0100

    reduction de la place eeprom

--- ESP32_WS_003/EEPROM.ino
+++ ESP32_WS_003/EEPROM.ino
@@ -1,13 +1,13 @@
 #include <EEPROM.h>
 
 void eeprom_setID(byte id) {
-  EEPROM.begin(512);
+  EEPROM.begin(64);
   EEPROM.write(0, id);
   EEPROM.end();
 }
 
 byte eeprom_getID() {
-  EEPROM.begin(512);
+  EEPROM.begin(64);
   byte id = EEPROM.read(0);
   EEPROM.end();
   return id;
