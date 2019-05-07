a8f7efdd2b375eb95d8bf8d8018f5bcc0f381183
richard fontaine <ririfonfon10@gmail.com>
2019-05-07 17:41:48 +0200

    remplacement do_effect* par effet
    Decoding stack results
    0x4008cb08: invoke_abort at /Users/ficeto/Desktop/ESP32/ESP32/esp-idf-public/components/esp32/panic.c line 155
    0x4008cd39: abort at /Users/ficeto/Desktop/ESP32/ESP32/esp-idf-public/components/esp32/panic.c line 170
    0x400ec4ab: task_wdt_isr at /Users/ficeto/Desktop/ESP32/ESP32/esp-idf-public/components/esp32/task_wdt.c line 174
    0x400d27e5: isActive(int, int) at F:\github\ESP32_WS\ESP32_WS_007_4/effet.ino line 51
    0x400d280f: effet() at F:\github\ESP32_WS\ESP32_WS_007_4/effet.ino line 71
    0x400d35f7: effTask(void*) at F:\github\ESP32_WS\ESP32_WS_007_4/X_task.ino line 189
    0x40088aa5: vPortTaskWrapper at /Users/ficeto/Desktop/ESP32/ESP32/esp-idf-public/components/freertos/port.c line 143

---
+++ ESP32_WS_007_4/leds.ino
@@ -0,0 +1,89 @@
+
+int test_led_niv = 50;
+
+void leds_show() {
+  for (int strip = 0 ; strip < NUM_STRIPS ; strip++)
+    digitalLeds_updatePixels(strands[strip]);
+}//leds_show
+
+void black() {
+
+  for (int strip = 0 ; strip < NUM_STRIPS ; strip++)
+    for (int i = 0 ; i < NUM_LEDS_PER_STRIP ; i++)
+      strands[strip]->pixels[i] = pixelFromRGB(0, 0, 0);
+}//black
+
+void ledblack() {
+
+  for (int strip = 0 ; strip < NUM_STRIPS ; strip++)
+    for (int i = 0 ; i < NUM_LEDS_PER_STRIP ; i++) {
+      pix_buffer[strip][i] = pixelFromRGB(0, 0, 0);
+      strands[strip]->pixels[i] = pixelFromRGB(0, 0, 0);
+    }//for i
+  leds_show();
+}//ledBlack
+
+int linear(int val) {
+  return (val * val) / 255;
+}
+
+void initTest() {
+
+  black();
+  leds_show();
+  delay(500);
+  for (int strip = 0 ; strip < NUM_STRIPS ; strip++)
+    for (int i = 0 ; i < NUM_LEDS_PER_STRIP ; i++)
+      strands[strip]->pixels[i] = pixelFromRGB(test_led_niv, 0, 0);
+  leds_show();
+  delay(500);
+  for (int strip = 0 ; strip < NUM_STRIPS ; strip++)
+    for (int i = 0 ; i < NUM_LEDS_PER_STRIP ; i++)
+      strands[strip]->pixels[i] = pixelFromRGB(0, test_led_niv, 0);
+  leds_show();
+  delay(500);
+  for (int strip = 0 ; strip < NUM_STRIPS ; strip++)
+    for (int i = 0 ; i < NUM_LEDS_PER_STRIP ; i++)
+      strands[strip]->pixels[i] = pixelFromRGB(0, 0, test_led_niv);
+  leds_show();
+  delay(500);
+  black();
+  leds_show();
+}//initest
+
+void gpioSetup(int gpioNum, int gpioMode, int gpioVal) {
+#if defined(ARDUINO) && ARDUINO >= 100
+  pinMode (gpioNum, gpioMode);
+  digitalWrite (gpioNum, gpioVal);
+#elif defined(ESP_PLATFORM)
+  gpio_num_t gpioNumNative = static_cast<gpio_num_t>(gpioNum);
+  gpio_mode_t gpioModeNative = static_cast<gpio_mode_t>(gpioMode);
+  gpio_pad_select_gpio(gpioNumNative);
+  gpio_set_direction(gpioNumNative, gpioModeNative);
+  gpio_set_level(gpioNumNative, gpioVal);
+#endif
+}// gpioSetup
+
+
+void leds_init() {
+  for (int k = 0; k < NUM_STRIPS; k++) {
+    STRANDS[k] = {  .rmtChannel = k, .gpioNum = PINS[k], .ledType = LED_WS2812B_V3, .brightLimit = 32,
+                    .numPixels = NUM_LEDS_PER_STRIP, .pixels = nullptr, ._stateVars = nullptr
+                 };
+    gpioSetup(PINS[k], OUTPUT, LOW);
+  }
+  int STRANDCNT = sizeof(STRANDS) / sizeof(STRANDS[0]);
+  if (digitalLeds_initStrands(STRANDS, STRANDCNT)) {
+#ifdef DEBUG
+    Serial.println("Init FAILURE: halting");
+    delay(1000);
+#endif
+    ESP.restart();
+  }
+  for (int i = 0; i < STRANDCNT; i++) {
+    strand_t * pStrand = &STRANDS[i];
+  }
+#ifdef DEBUG
+  Serial.println("Init complete");
+#endif
+}//leds_init
